b0ff87b6455da0da0561f51ad7468dec0d9fb648
Spheya <tamara@spheya.com>
2024-06-15 12:34:33 +0200

    add vertex default constructor

--- src/luna/Vertex.hpp
+++ src/luna/Vertex.hpp
@@ -4,7 +4,7 @@
 
 namespace luna {
 	struct Vertex {
-		GLM_CONSTEXPR explicit Vertex(glm::vec3 position, glm::vec2 uv = glm::vec2(0.0f), glm::vec3 normal = glm::vec3(0.0f), uint32_t color = 0xffffffff) :
+		GLM_CONSTEXPR explicit Vertex(glm::vec3 position = glm::vec3(0.0f), glm::vec2 uv = glm::vec2(0.0f), glm::vec3 normal = glm::vec3(0.0f), uint32_t color = 0xffffffff) :
 			position(position),
 			uv(uv),
 			normal(normal),
